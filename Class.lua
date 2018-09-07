4aa732dd9eee97ac4f912d9f5ce6416f84b0a543
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2018-09-07 17:52:39 +0930

    Add default constructor, and super

--- Class.lua
+++ Class.lua
@@ -1,18 +1,39 @@
 local CreateClass = function(parent)
 	local class = {}
+
+	function class.constructor(...)
+		if (parent and parent.constructor) then
+			parent.constructor(...)
+		end
+	end
+
 	local classType = {
-		__index = {
-			new = function(...)
-				local args = {...}
-				local object = setmetatable({}, {__index = class})
+		__index = function(class, key)
+			if (key == 'new') then
+				return function(...)
+					local object =
+						setmetatable(
+						{},
+						{
+							__index = function(object, key)
+								if (key == 'super') then
+									return parent
+								end
+								return class[key]
+							end
+						}
+					)
 
-				if (class.constructor ~= nil) then
-					class.constructor(object, ...)
+					if (class.constructor ~= nil) then
+						class.constructor(object, ...)
+					end
+
+					return object
 				end
-
-				return object
+			else
+				return rawget(class, key) or parent and parent[key]
 			end
-		}
+		end
 	}
 	setmetatable(class, classType)
 	return class
