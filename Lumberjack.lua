77048057b879ff0199e19c9f5dd6dd4b9bbd450b
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2018-09-08 17:42:29 +0930

    Add lumberjack file

---
+++ Lumberjack.lua
@@ -0,0 +1,44 @@
+local Lumberjack = {}
+local log = Log.new('lumberjack')
+
+local function collectLeaves()
+	for i = 1, 4 do
+		turtle.dig()
+		turtle.turnRight()
+	end
+end
+
+function Lumberjack.harvestTree()
+	log:info('Harvesting Tree')
+	turtle.dig()
+	turtle.forward()
+	local height = 0
+
+	while (turtle.digUp()) do
+		turtle.up()
+		height = height + 1
+
+		collectLeaves()
+	end
+
+	while (height > 0) do
+		turtle.down()
+		heght = height - 1
+	end
+
+	turtle.back()
+end
+
+function Lumberjack.plantTree()
+	--TODO: select sapling
+	return turtle.place()
+end
+
+local args = {...}
+if (#args > 0) then
+	if (Lumberjack[args[1]]) then
+		Lumberjack[args[1]]()
+	end
+else
+	return Lumberjack
+end
