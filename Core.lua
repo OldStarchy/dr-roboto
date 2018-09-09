e86ebdd92db5353948bf771fc836b30047a35c26
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2018-09-09 20:29:49 +0930

    Reorder file load order

--- Core.lua
+++ Core.lua
@@ -1,20 +1,21 @@
 require 'Class'
 
+--Classes
 require 'Log'
-
 require 'Crafting/Recipe'
 require 'Crafting/RecipeBook'
-require 'Crafting/StandardRecipes'
-
-require 'Crafting/Crafting'
-
-require 'Inventory/ItemStorage'
-
 require 'Navigation/Position'
+
+require 'Skills/Skill'
+require 'Skills/SkillSet'
+
+require 'Skills/TreeFarmSkill'
+
+--Mods / APIs
+require 'Crafting/Crafting'
+require 'Inventory/ItemStorage'
 require 'Navigation/Gps'
 require 'Navigation/Navigator'
 
-require 'Skills/Skill'
-require 'Skills/TreeFarmSkill'
-
-require 'Skills/SkillSet'
+--Variables / Constants
+require 'Crafting/StandardRecipes'
