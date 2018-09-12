8fada3c98ed68a04006295f1e250cb0a5ac0f885
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2018-09-12 19:55:06 +0930

    Add some tests for recipe and book

--- Crafting/tests/RecipeBookTest.lua
+++ Crafting/tests/RecipeBookTest.lua
@@ -1,8 +1,40 @@
 test(
 	'RecipeBook',
 	{
-		Constructor = function(t)
-			RecipeBook.new()
+		['find'] = function(t)
+			local book = RecipeBook.new()
+
+			local matches = book:findByName('item1')
+
+			t.assertEqual(type(matches), 'table')
+		end,
+		['find on empty'] = function(t)
+			local book = RecipeBook.new()
+
+			local matches = book:findByName('item1')
+
+			t.assertEqual(#matches, 0)
+		end,
+		['find with match'] = function(t)
+			local book = RecipeBook.new()
+
+			local recipe = Recipe.new('item1', {}, 1)
+			book:add(recipe)
+
+			local matches = book:findByName('item1')
+
+			t.assertEqual(#matches, 1)
+			t.assertEqual(matches[1], recipe)
+		end,
+		['find without match'] = function(t)
+			local book = RecipeBook.new()
+
+			local recipe = Recipe.new('item1', {}, 1)
+			book:add(recipe)
+
+			local matches = book:findByName('potatoes')
+
+			t.assertEqual(#matches, 0)
 		end
 	}
 )
--- Crafting/tests/RecipeTest.lua
+++ Crafting/tests/RecipeTest.lua
@@ -1,23 +1,16 @@
 test(
 	'Recipe',
 	{
-		Constructor = {
-			['Dummy'] = function(t)
-				local obj = Recipe.new('dummy', {'uniuqeingredient'}, 1)
+		['Item Count'] = function(t)
+			local obj = Recipe.new('item1', {'item2', 'item3', 'item3'}, 1)
 
-				--TODO: this should throw, since the recipe {'log'} is taken by planks
-			end,
-			['Correct Item Count'] = function(t)
-				local obj = Recipe.new('stick', {'plank', nil, nil, 'plank'}, 4)
+			t.assertEqual(obj.itemCount, 3)
+		end,
+		['Specific Item Count'] = function(t)
+			local obj = Recipe.new('item1', {'item2', 'item3', 'item3'}, 1)
 
-				t.assertEqual(obj.itemCount, 2)
-			end,
-			['Correct Items Required'] = function(t)
-				local obj = Recipe.new('stick', {'plank', nil, nil, 'plank', 'log'}, 4)
-				t.assertTableEqual(obj.items, {['plank'] = 2, ['log'] = 1})
-			end
-		}
-
-		--TODO: more tests
+			t.assertEqual(obj.items['item2'], 1)
+			t.assertEqual(obj.items['item3'], 2)
+		end
 	}
 )
