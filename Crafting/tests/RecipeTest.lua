eee820c501a458bb8a1c542ca1753348c5d99733
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2018-09-14 20:54:40 +0930

    Restore tests i accidentally nuked

--- Crafting/tests/RecipeTest.lua
+++ Crafting/tests/RecipeTest.lua
@@ -1,27 +1,23 @@
 test(
 	'Recipe',
 	{
-		['Item Count'] = function(t)
-			local obj = Recipe.new('item1', {'item2', 'item3', 'item3'}, 1)
+		Constructor = {
+			['Item Count'] = function(t)
+				local obj = Recipe.new('item1', {'item2', 'item3', 'item3'}, 1)
 
-			t.assertEqual(obj.itemCount, 3)
-		end,
-		['Specific Item Count'] = function(t)
-			local obj = Recipe.new('item1', {'item2', 'item3', 'item3'}, 1)
+				t.assertEqual(obj.itemCount, 3)
+			end,
+			['Correct Item Count'] = function(t)
+				local obj = Recipe.new('stick', {'plank', nil, nil, 'plank'}, 4)
 
-			t.assertEqual(obj.items['item2'], 1)
-			t.assertEqual(obj.items['item3'], 2)
-		end,
-		['Item Count with nil'] = function(t)
-			local obj = Recipe.new('item1', {'item2', nil, 'item3'}, 1)
+				t.assertEqual(obj.itemCount, 2)
+			end,
+			['Correct Items Required'] = function(t)
+				local obj = Recipe.new('stick', {'plank', nil, nil, 'plank', 'log'}, 4)
+				t.assertTableEqual(obj.items, {['plank'] = 2, ['log'] = 1})
+			end
+		}
 
-			t.assertEqual(obj.itemCount, 2)
-		end,
-		['Specific Item Count with nil'] = function(t)
-			local obj = Recipe.new('item1', {'item2', nil, 'item3'}, 1)
-
-			t.assertEqual(obj.items['item2'], 1)
-			t.assertEqual(obj.items['item3'], 1)
-		end
+		-- TODO: more tests
 	}
 )
