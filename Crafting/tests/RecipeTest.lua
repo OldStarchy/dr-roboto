e23923deae1e792bd52ef3ce7b309d83599d0bf4
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2018-09-14 19:28:32 +0930

    Add a couple tests for recipe

--- Crafting/tests/RecipeTest.lua
+++ Crafting/tests/RecipeTest.lua
@@ -11,6 +11,17 @@ test(
 
 			t.assertEqual(obj.items['item2'], 1)
 			t.assertEqual(obj.items['item3'], 2)
+		end,
+		['Item Count with nil'] = function(t)
+			local obj = Recipe.new('item1', {'item2', nil, 'item3'}, 1)
+
+			t.assertEqual(obj.itemCount, 2)
+		end,
+		['Specific Item Count with nil'] = function(t)
+			local obj = Recipe.new('item1', {'item2', nil, 'item3'}, 1)
+
+			t.assertEqual(obj.items['item2'], 1)
+			t.assertEqual(obj.items['item3'], 1)
 		end
 	}
 )
