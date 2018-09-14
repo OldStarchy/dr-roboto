5aa67c48fce94df84f71f35d82c9b9581a991148
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2018-09-14 23:33:04 +0930

    Fix recipebook test

--- Crafting/tests/RecipeBookTest.lua
+++ Crafting/tests/RecipeBookTest.lua
@@ -17,7 +17,7 @@ test(
 			t.assertEqual(book:add(recipe1), true)
 			t.assertEqual(book:add(recipe2), false)
 
-			t.assertEqual(book:findByName('anotherthing'), nil)
+			t.assertEqual(#book:findByName('anotherthing'), 0)
 		end,
 		['find on empty'] = function(t)
 			local book = RecipeBook()
