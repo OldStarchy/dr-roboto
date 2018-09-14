524dfd5e0fd00e2d6e98306a7ffd6a07e1799a55
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2018-09-14 23:46:21 +0930

    Try to prevent infinite loops in programs that rely on turtle.up / down returning false sometimes

--- _polyfills/turtle.lua
+++ _polyfills/turtle.lua
@@ -1,13 +1,25 @@
 -- TODO: restructure test.lua and use the mock object defined there
 -- t.mock('turtle', true, 5) for equivalent behavior
+local depth = 64
 turtle =
 	setmetatable(
 	{},
 	{
 		__index = function(t, v)
 			return function(...)
+				if (v == 'down') then
+					if (depth <= 0) then
+						return false
+					end
+					depth = depth - 1
+				elseif (v == 'up') then
+					if (depth >= 256) then
+						return false
+					end
+					depth = depth + 1
+				end
 				print('called turtle.' .. v .. '(', unpack({...}), ')')
-				printStackTrace(5, 2)
+				-- printStackTrace(5, 2)
 				return true
 			end
 		end
