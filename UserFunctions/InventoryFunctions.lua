4cb190f016e90d410c27c68000f2a86f7bb04e8b
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2018-09-25 20:07:50 +0930

    Fix InventoryFunctions was not updated when renaming inventoryManager

--- UserFunctions/InventoryFunctions.lua
+++ UserFunctions/InventoryFunctions.lua
@@ -1,9 +1,9 @@
 registerUserFunction(
 	function(itemOrIndex, selector)
 		if (tonumber(itemOrIndex) ~= nil) then
-			print(Inventory.ItemIs(tonumber(itemOrIndex), selector))
+			print(InventoryManager.ItemIs(tonumber(itemOrIndex), selector))
 		else
-			print(Inventory.ItemIs(itemOrIndex, selector))
+			print(InventoryManager.ItemIs(itemOrIndex, selector))
 		end
 	end,
 	'itemIs',
