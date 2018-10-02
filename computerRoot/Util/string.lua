eab65a25456f832fc4e58952a2d34fb0cee13453
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2018-10-02 18:12:14 +0930

    Add more string util functions

--- computerRoot/Util/string.lua
+++ computerRoot/Util/string.lua
@@ -1,7 +1,15 @@
 function string:startsWith(start)
-	return self:sub(1, #start) == start
+	return start == '' or self:sub(1, #start) == start
 end
 
 function string:endsWith(ending)
 	return ending == '' or self:sub(-(#ending)) == ending
+end
+
+function string:isLower()
+	return self:lower() == self
+end
+
+function string:isUpper()
+	return self:upper() == self
 end
