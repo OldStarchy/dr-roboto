7b364ed59c471b544ea6c41d2d7a389387dcb140
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2018-10-21 09:48:19 +1030

    Add some log helper methods and some doc

--- computerRoot/Core/Log.lua
+++ computerRoot/Core/Log.lua
@@ -5,10 +5,23 @@ Log.INFO = 0
 Log.WARNING = 1
 Log.ERROR = 2
 
+Log.levels = {
+	[0] = 'I',
+	[1] = 'W',
+	[2] = 'E'
+}
+
+--[[
+	name - A label to appear next to ech log
+	level - The minimum log level to log
+	output - An output function like term.write that will not append newlines automatically
+]]
 function Log:constructor(name, level, output)
 	self.name = name
 	self.level = level or Log.INFO
-	self.output = output or print
+	self.output = output or function(...)
+			io.write(...)
+		end
 end
 
 function Log:log(level, ...)
@@ -18,10 +31,12 @@ function Log:log(level, ...)
 
 	if (level >= self.level) then
 		if (self.name) then
-			self.output(self.name .. '] ')
+			self.output(self.name .. '|' .. Log.levels[level] .. '] ')
 		end
 
 		self.output(...)
+
+		self.output('\n')
 	end
 end
 
@@ -50,3 +65,11 @@ function Log:error(...)
 end
 
 Log.default = Log('', Log.INFO)
+
+function info(...)
+	Log.default:info(...)
+end
+
+function warning(...)
+	Log.default:warning(...)
+end
