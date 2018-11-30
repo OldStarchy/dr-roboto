72071a576af296c17a502924f75d1bf9ca7fcea5
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2018-11-30 21:50:29 +1030

    Add funrace recipe support to recipe book

--- computerRoot/Crafting/RecipeBook.lua
+++ computerRoot/Crafting/RecipeBook.lua
@@ -2,46 +2,66 @@ RecipeBook = Class()
 RecipeBook.ClassName = 'RecipeBook'
 
 function RecipeBook:constructor()
-	self._recipes = {}
+	self._craftingRecipes = {}
+	self._furnaceRecipes = {}
 end
 
 function RecipeBook:add(recipe)
-	if (recipe.getType() ~= Recipe) then
-		error('Attempt to add a non recipe to a book', 2)
+	assertType(recipe, Recipe, 'Attempt to add a non recipe to a book', 2)
+
+	local rt = recipe:getType()
+
+	--TODO: Recipe should be base of CraftingRecipe and FurnaceRecipe
+	if (rt == Recipe) then
+		if (self:findByGrid(recipe.grid) ~= nil) then
+			return false
+		end
+	elseif (isType(rt, FurnaceRecipe)) then
+		if (self:findByIngredient(recipe.ingredient) ~= nil) then
+			return false
+		end
+	else
+		error('unknown recipe type', 2)
 	end
 
-	if (self:findByGrid(recipe.grid) ~= nil) then
-		return false
-	end
-
-	table.insert(self._recipes, recipe)
+	table.insert(self._craftingRecipes, recipe)
 	return true
 end
 
 function RecipeBook:findByName(name)
 	local recipes = {}
 
-	for i = 1, #self._recipes do
-		if (self._recipes[i].name == name) then
-			table.insert(recipes, self._recipes[i])
+	for i = 1, #self._craftingRecipes do
+		if (self._craftingRecipes[i].name == name) then
+			table.insert(recipes, self._craftingRecipes[i])
 		end
 	end
 
 	return recipes
 end
 
+function RecipeBook:findByIngredient(ingredient)
+	for i = 1, #self._furnaceRecipes do
+		if (self._furnaceRecipes[i].ingredient ~= ingredient) then
+			return self._furnaceRecipes[i]
+		end
+	end
+
+	return nil
+end
+
 function RecipeBook:findByGrid(recipe)
-	for i = 1, #self._recipes do
+	for i = 1, #self._craftingRecipes do
 		local match = true
 		for j = 1, 9 do
-			if (self._recipes[i].grid[j] ~= recipe[j]) then
+			if (self._craftingRecipes[i].grid[j] ~= recipe[j]) then
 				match = false
 				break
 			end
 		end
 
 		if (match) then
-			return self._recipes[i]
+			return self._craftingRecipes[i]
 		end
 	end
 
