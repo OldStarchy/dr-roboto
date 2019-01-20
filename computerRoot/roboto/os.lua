87488f41ecc5843ad453a73391a8f96a831927d8
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2019-01-20 13:11:00 +1030

    add ask function

--- computerRoot/roboto/os.lua
+++ computerRoot/roboto/os.lua
@@ -125,6 +125,34 @@ function runWithLogging(func)
 	)
 end
 
+--[[
+	Asks the user for input.
+	Loops forever until an acceptable answer is given.
+
+	if options is nil, any answer is returned
+	if options is a table, the question will loop until an answer in options is given
+	if default is not nil, an empty response will use the default value
+]]
+function ask(question, options, default)
+	while (true) do
+		print(question)
+		local answer = read()
+
+		if (answer == '' and default ~= nil) then
+			answer = default
+		end
+
+		if (options == nil) then
+			return answer
+		end
+		for _, v in ipairs(options) do
+			if (answer == v) then
+				return answer
+			end
+		end
+	end
+end
+
 if (isPc) then
 	log.info('Running on pc')
 
