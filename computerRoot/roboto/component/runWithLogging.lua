4bf337e0e6da457689f854afc4eceb83b9efe25b
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2019-02-10 00:29:46 +1030

    Hopefully better logging in runWithLogging

--- computerRoot/roboto/component/runWithLogging.lua
+++ computerRoot/roboto/component/runWithLogging.lua
@@ -1,12 +1,18 @@
 function runWithLogging(func)
+	local stopFrame = getStackFrameInfo(1)
+
 	return xpcall(
 		func,
 		function(err)
 			local trace = getStackTrace(20, 2)
 			trace[1] = err
 
-			for _, err in pairs(trace) do
+			for _, err in ipairs(trace) do
 				local frameInfo = getStackFrameInfo(err)
+
+				if (frameInfo.file == stopFrame.file) then
+					break
+				end
 
 				local errLine =
 					stringutil.join(
@@ -23,7 +29,7 @@ function runWithLogging(func)
 				if (frameInfo.file and frameInfo.line) then
 					errLine = getFileLines(frameInfo.file, frameInfo.line, 3)
 					if (errLine ~= nil) then
-						log.error('\t' .. string.gsub(errLine, '\n', '\n\t') .. '\n\n')
+						log.error('\n\t' .. string.gsub(errLine, '\n', '\n\t') .. '\n\n')
 					end
 				end
 			end
