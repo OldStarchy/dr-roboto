eed7dca753a61382bdc6627edac157d40d8ac291
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2019-02-17 02:45:18 +1030

    Improvements to the fs polyfil
    fs.exists was using lfs.attributes which is case insensitive. This way is slower but actually works

--- _polyfills/fs.lua
+++ _polyfills/fs.lua
@@ -3,8 +3,16 @@ _G.fs = {
 	list = function(directory)
 		local dirs = {}
 
+		if (directory == nil or directory == '') then
+			directory = '.'
+		else
+			directory = './' .. directory
+		end
+
 		for d in lfs.dir(directory) do
-			table.insert(dirs, d)
+			if (d ~= '.' and d ~= '..') then
+				table.insert(dirs, d)
+			end
 		end
 
 		return dirs
@@ -16,7 +24,23 @@ _G.fs = {
 		return directory:sub(unpack({directory:find('[^/]+$')}))
 	end,
 	exists = function(directory)
-		return lfs.attributes(directory) ~= nil
+		directory = fs.combine('.', directory)
+
+		if (directory == '' or directory == '.' or directory == '/') then
+			return true
+		end
+
+		local parentDir = fs.combine(directory, '..')
+
+		local content = fs.list(parentDir)
+
+		for _, v in ipairs(content) do
+			local fname = fs.combine(parentDir, v)
+			if (fname == directory) then
+				return true
+			end
+		end
+		return false
 	end,
 	open = function(filename, args)
 		local f = io.open(filename, args)
@@ -64,5 +88,39 @@ _G.fs = {
 	end,
 	makeDir = function(dest)
 		return lfs.mkdir(dest)
+	end,
+	combine = function(a, b)
+		local fullPath
+
+		if (a == nil and b == nil) then
+			error('must specify paths to combine')
+		end
+		if (a == nil) then
+			fullPath = b
+		elseif (b == nil) then
+			fullPath = a
+		else
+			fullPath = a .. '/' .. b
+		end
+
+		local parts = stringutil.split(fullPath, '/')
+
+		local result = {}
+
+		for i, v in ipairs(parts) do
+			if (v ~= '.') then
+				if (v == '..') then
+					if (#result == 0) then
+						return nil
+					end
+
+					table.remove(result)
+				elseif (v ~= '') then
+					table.insert(result, v)
+				end
+			end
+		end
+
+		return table.concat(result, '/')
 	end
 }
