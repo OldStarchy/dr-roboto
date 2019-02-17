eb1e9462f8f638080ac7b5adcb9ff821301068d6
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2019-02-17 13:42:23 +1030

    add search path to shell

--- computerRoot/programs/Shell.lua
+++ computerRoot/programs/Shell.lua
@@ -1,6 +1,16 @@
 local path = ''
 local run = true
 local foregroundColor = colours.yellow
+local internalCommands
+
+local shellEnvironment
+
+local function getSearchPath()
+	return {
+		path,
+		'rom/programs'
+	}
+end
 
 local function printPrompt()
 	term.setTextColour(foregroundColor)
@@ -14,14 +24,73 @@ local function resolvePath(dir)
 	end
 
 	if (dir:sub(1, 1) == '/') then
-		path = dir:sub(2)
-		return path
+		return dir:sub(2)
 	end
 
 	return fs.combine(path, dir)
 end
 
-local internalCommands = {
+local function searchPath(dir)
+	if (dir:sub(1, 1) == '/') then
+		return dir:sub(2)
+	end
+
+	for _, pth in ipairs(getSearchPath()) do
+		local potential = fs.combine(pth, dir)
+		if (fs.exists(potential)) then
+			return potential
+		end
+	end
+
+	return nil
+end
+
+local function resolveCommand(cmd)
+	local localFile = searchPath(cmd)
+
+	if (localFile ~= nil and fs.exists(localFile)) then
+		return loadfile(localFile, shellEnvironment)
+	end
+
+	if (internalCommands[cmd]) then
+		return internalCommands[cmd]
+	end
+
+	return nil
+end
+
+local function runCommand(input)
+	--TODO: doesn't handle quoted strings
+	local args = stringutil.split(input, ' ')
+
+	local cmd = table.remove(args, 1)
+	if (cmd == nil) then
+		return
+	end
+
+	local command = resolveCommand(cmd)
+
+	if (command ~= nil) then
+		command(unpack(args))
+	else
+		print('"' .. cmd .. '" is not an internal or external command')
+	end
+end
+
+shellEnvironment = {
+	shell = {
+		dir = function()
+			return path
+		end,
+		resolve = function(dir)
+			return resolvePath(dir)
+		end
+	}
+}
+
+setmetatable(shellEnvironment, {__index = _G})
+
+internalCommands = {
 	exit = function()
 		print('Exiting shell')
 		run = false
@@ -51,43 +120,16 @@ local internalCommands = {
 		for _, v in ipairs(files) do
 			print(v)
 		end
+	end,
+	which = function(dir)
+		if (dir ~= nil) then
+			print(searchPath(dir))
+		end
 	end
 }
 
 if (isDefined('interactiveLua')) then
 	internalCommands.lua = interactiveLua
-end
-
-local function resolveCommand(cmd)
-	local localFile = fs.combine(path, cmd)
-
-	if (fs.exists(localFile)) then
-		return loadfile(localFile, _G)
-	end
-
-	if (internalCommands[cmd]) then
-		return internalCommands[cmd]
-	end
-
-	return nil
-end
-
-local function runCommand(input)
-	--TODO: doesn't handle quoted strings
-	local args = stringutil.split(input, ' ')
-
-	local cmd = table.remove(args, 1)
-	if (cmd == nil) then
-		return
-	end
-
-	local command = resolveCommand(cmd)
-
-	if (command ~= nil) then
-		command(unpack(args))
-	else
-		print('"' .. cmd .. '" is not an internal or external command')
-	end
 end
 
 while (run) do
