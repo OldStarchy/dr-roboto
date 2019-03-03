f032a48d85b4650ca0d95a3088faa2fdb38c4aa2
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2019-03-04 00:01:59 +1030

    Allow manually creating itemstackdetails and add some error checking

--- computerRoot/Inventory/ItemDetail/ItemStackDetail.lua
+++ computerRoot/Inventory/ItemDetail/ItemStackDetail.lua
@@ -1,10 +1,23 @@
 ItemStackDetail = Class(ItemDetail)
 ItemStackDetail.ClassName = 'ItemStackDetail'
 
-function ItemStackDetail:constructor()
-	error("Can't construct items manually", 3)
+--[[
+	name
+	damage
+	metadata
+	count
+]]
+function ItemStackDetail:constructor(name, metadata, count)
+	self.name = assertType(name, 'string')
+	self.metadata = assertType(coalesce(metadata, 0), 'int')
+	self.damage = self.metadata
+	self.count = assertType(coalesce(count, 1), 'int')
 end
 
 function ItemStackDetail:conversionConstructor()
+	assertType(self.name, 'string')
+	assertType(self.damage, 'int')
+	assertType(self.count, 'int')
+
 	self.metadata = self.damage
 end
