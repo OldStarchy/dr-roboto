f27ff7ab0021122cfbaf39da71eee3ebcef86519
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2019-03-04 00:28:39 +1030

    Remove name property of block

--- computerRoot/Core/Block.lua
+++ computerRoot/Core/Block.lua
@@ -2,37 +2,34 @@ Block = Class()
 Block.ClassName = 'Block'
 
 --[[
-	name: name of the item, 'chest', 'furnace', or block query that is used to select the item from inventory
 	location: Position
 
 	where the direction provided in Position will be the approach direction
 	from the turtle to the block
 ]]
-function Block:constructor(name, location)
+function Block:constructor(location)
 	assertType(location, Position, 'Location must be of type Position', 2)
 
 	self.location = location
-	self.name = name
 
 	self.interfaceLocation = Position(location.x, location.y, location.z, location.direction)
 	self.interfaceLocation:sub(Position.Offsets[location.direction])
 end
 
 function Block:toString()
-	return 'Block: ' ..
-		tostring(self.name) ..
-			' at location: ' .. self.location:toString() .. ', interface location: ' .. self.interfaceLocation:toString()
+	return self.ClassName ..
+		': ' .. ' at location: ' .. self.location:toString() .. ', interface location: ' .. self.interfaceLocation:toString()
 end
 
--- TODO: make smarted
+-- TODO: move to Navigator
 function Block:navigateTo()
 	nav:goTo(self.interfaceLocation.x, self.interfaceLocation.y, self.interfaceLocation.z)
 	mov:face(self.interfaceLocation.direction)
 end
 
--- TODO: make smarted
-function Block:place()
-	self:navigateTo()
-	inv:select(name)
-	turtle.place()
-end
+-- TODO: Move some block placer class
+-- function Block:place()
+-- 	self:navigateTo()
+-- 	inv:select(name)
+-- 	turtle.place()
+-- end
