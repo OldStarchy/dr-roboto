834fa3406351b21b450f1d0fce5a792c6dae092d
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2019-03-04 20:52:41 +1030

    Move inventory storage code out of Chest and into ItemStore

--- computerRoot/Inventory/ItemStore.lua
+++ computerRoot/Inventory/ItemStore.lua
@@ -1,22 +1,34 @@
 ItemStore = Class()
 ItemStore.ClassName = 'ItemStore'
 
-function ItemStore:constructor()
+function ItemStore:constructor(size)
+	assertType(size, 'int')
+
+	if (size < 1) then
+		error('Invalid storage size "' .. tostring(size) .. '"', 2)
+	end
+
+	self._size = size
+
 	self._items = {}
 
-	self._stack = {}
+	self._stateStack = {}
 end
 
-function ItemStore:push()
-	table.insert(self._stack, cloneTable(self._items, 2))
+function ItemStore:pushState()
+	table.insert(self._stateStack, cloneTable(self._items, 2))
 end
 
-function ItemStore:pop()
-	assert(#self._stack > 0, 'Too many calls to ItemStore:pop')
+function ItemStore:popState()
+	if (#self._stateStack == 0) then
+		error('Too many calls to ItemStore:popState', 2)
+	end
 
-	local oldItems = self._items
-	self._items = table.remove(self._stack)
-	return oldItems
+	self._items = table.remove(self._stateStack)
+end
+
+function ItemStore:size()
+	return self._size
 end
 
 function ItemStore:add(item, count)
@@ -35,7 +47,7 @@ end
 
 function ItemStore:remove(itemSelector, count)
 	assertType(itemSelector, 'string')
-	self:push()
+	self:pushState()
 	local removed = {}
 
 	local i = 1
@@ -62,7 +74,7 @@ function ItemStore:remove(itemSelector, count)
 		end
 
 		if (count == 0) then
-			self._items = self:pop()
+			self._items = self:popState()
 			return removed
 		end
 
@@ -70,9 +82,309 @@ function ItemStore:remove(itemSelector, count)
 	end
 
 	if (count > 0) then
-		self:pop()
+		self:popState()
 		return false
 	end
 
 	return removed
 end
+
+function ItemStore:isEmpty()
+	if
+		(self:getFirst(
+			function(self, slot)
+				return self:getItemCount(slot) ~= 0
+			end
+		))
+	 then
+		return false
+	end
+
+	return true
+end
+
+function ItemStore:getFirst(delegate)
+	return self:getNext(delegate, 0)
+end
+
+function ItemStore:getNext(delegate, last)
+	last = last or 0
+	for i = last + 1, self:size() do
+		if delegate(self, i) then
+			return i
+		end
+	end
+	return nil
+end
+
+function ItemStore:getLast(delegate) --GOOD
+	for i = self:size(), 1, -1 do
+		if delegate(self, i) then
+			return i
+		end
+	end
+	return nil
+end
+
+function ItemStore:getPrevious(delegate, last) --GOOD
+	last = last or self:size() + 1
+	for i = last - 1, 1, -1 do
+		if delegate(self, i) then
+			return i
+		end
+	end
+	return nil
+end
+
+function ItemStore:getItemAt(slot)
+	if self._items[slot] == nil then
+		return nil
+	else
+		return self._items[slot].name
+	end
+end
+
+function ItemStore:getItemCount(slot)
+	return ((self._items[slot] and self._items[slot].count) or 0)
+end
+
+function ItemStore:getItemSpace(slot, item)
+	local _item = self:getItemAt(slot) or item
+	local stackSize = ItemInfo.Instance:getStackSize(_item)
+	local items = self:getItemCount(slot)
+
+	return stackSize - items
+end
+
+--[[
+	return the first avaiable index of a free slot for the provided item
+	]]
+function ItemStore:firstAvailable(item)
+	return self:nextAvailable(item, 0)
+end
+
+--[[
+	return the next avaiable index of a free slot for the provided item and starting index
+	]]
+function ItemStore:nextAvailable(item, from)
+	from = from or 0
+	for i = from + 1, self:size() do
+		if self._items[i] == nil then
+			return i, ItemInfo.Instance:getStackSize(item)
+		elseif self._items[i].name == item then
+			if (ItemInfo.Instance:getStackSize(item) - self._items[i].count > 0) then
+				return i, ItemInfo.Instance:getStackSize(item) - self._items[i].count
+			end
+		end
+	end
+	return nil
+end
+
+--[[
+	returns the total avaiable space in the chest for this item
+]]
+function ItemStore:getTotalSpaceFor(item)
+	local count = 0
+	local i = self:firstAvailable(item)
+	local spare = self:getItemSpace(i, item)
+
+	while i ~= nil do
+		count = count + spare
+		i = self:nextAvailable(item, i)
+		spare = self:getItemSpace(i, item)
+	end
+
+	return count
+end
+
+--[[
+	verifies if it can push an item into the chest
+]]
+function ItemStore:canPush(item, count)
+	assert(item ~= nil, 'cannot check for nil item')
+	assertType(count, 'int', 'count is required and a number')
+
+	if (count == 0) then
+		return true
+	end
+
+	local c = self:getTotalSpaceFor(item) - count
+	if c >= 0 then
+		return true
+	else
+		return false, -c
+	end
+end
+
+--[[
+	pushes an item into the chest at the next space available.
+	will return false, 'error desc' if failed.
+]]
+function ItemStore:push(item, count)
+	assert(item ~= nil, 'cannot check for nil item')
+	assertType(count, 'int', 'count is required and a number')
+
+	if (count == 0) then
+		return true
+	end
+
+	if (self:getTotalSpaceFor(item) < count) then
+		return false, 'Not enough space for items'
+	end
+	local i = self:firstAvailable(item)
+	spare = self:getItemSpace(i, item)
+	local moved = 0
+	while (i ~= nil and moved < count) do
+		if spare > (count - moved) then
+			spare = (count - moved)
+		end
+		if (self._items[i] == nil) then
+			--TODO metadata/damage needs to be stored
+			self._items[i] = ItemStackDetail(item, 0, spare)
+		else
+			self._items[i].count = self._items[i].count + spare
+		end
+		moved = moved + spare
+
+		i = self:nextAvailable(item, i)
+		spare = self:getItemSpace(i, item)
+	end
+	if (moved < count) then
+		return false, 'Not enough space for items (some moved)'
+	end
+	return true
+end
+
+--[[
+	returns true if the chest contains item by exact name
+]]
+function ItemStore:has(item)
+	for i = 1, self:size() do
+		if self._items[i] then
+			if self._items[i].name == item then
+				return true
+			end
+		end
+	end
+	return false
+end
+
+--[[
+	returns top item in chest stack
+	returns name, count
+]]
+function ItemStore:peek()
+	for i = 1, self:size() do
+		if self._items[i] then
+			return self._items[i].name, self._items[i].count
+		end
+	end
+end
+
+--[[
+	returns and removes top item in chest stack
+	returns name, count
+]]
+function ItemStore:pop()
+	local r = nil
+	for i = 1, self:size() do
+		if self._items[i] then
+			r = self._items[i]
+			self._items[i] = nil
+			break
+		end
+	end
+	return r.name, r.count
+end
+
+--[[
+	clears information about contents of chest
+]]
+function ItemStore:clear()
+	self._items = nil
+end
+
+function ItemStore:print(start)
+	start = start or 1
+	local lim = self:size()
+	if lim > start + 10 then
+		lim = start + 10
+	end
+	for i = start, lim do
+		if self._items[i] then
+			print(i .. ': ' .. self._items[i].count .. ' ' .. self._items[i].name)
+		end
+	end
+end
+
+--[[
+--Taken from the old CQL library. Don't think it really
+--belongs in ItemStore but not sure where to put it yet
+
+--Cycles through items in "this" chest putteng them into a
+--temp chest untill the requested slot has been reached, then
+--puts all the other items back in "this" chest
+
+function index(side, name, isDouble)
+	if not side then
+		side = ''
+	end
+	debugger.assert(side == '' or side == 'Up' or side == 'Down')
+	if not inv.push('minecraft:chest') then
+		inv.pop()
+		return nil, 'Need a chest'
+	end
+
+	if turtle.getItemCount() ~= 1 then
+		if not inv.push() then
+			inv.pop()
+			inv.pop()
+			return nil, 'Need an empty slot to cycle items'
+		end
+		inv.pop()
+	end
+
+	local chestOptions = {Up = true, Down = true, [''] = true}
+	chestOptions[side] = nil
+	local chestpos = nil
+	for i, v in pairs(chestOptions) do
+		if not turtle['detect' .. i]() then
+			chestPos = i
+			break
+		end
+	end
+
+	if not chestPos then
+		return nil, 'No room to place chest'
+	end
+
+	turtle['place' .. chestPos]()
+	inv.select()
+
+	local function cycleOut()
+		local a = turtle['suck' .. side]()
+		turtle['drop' .. chestPos]()
+		return a
+	end
+
+	local chest = Chest.new(name, isDouble)
+	local function cycleIn()
+		turtle['suck' .. chestPos]()
+		local deets = turtle.getItemDetail()
+		chest:push(deets.name .. tostring(deets.damage), deets.count)
+		turtle['drop' .. side]()
+	end
+
+	local stacks = 0
+	while cycleOut() and stacks < 27 do
+		stacks = stacks + 1
+	end
+
+	for i = 1, stacks do
+		cycleIn()
+	end
+
+	turtle['dig' .. chestPos]()
+	return chest
+end
+]]
--- computerRoot/Inventory/tests/ChestTest.lua
+++ computerRoot/Inventory/tests/ChestTest.lua
@@ -4,120 +4,13 @@ test(
 		Constructor = {
 			['Single Chest Constructor'] = function(t)
 				local p = Position(5, 5, 5, Position.SOUTH)
-				local chest = Chest(t.testName, p, false)
 
-				chest:remove()
+				local chest = Chest(p, false)
+				t.assertEqual(chest:inventory():size(), 27)
 
-				t.assertFinished()
-			end,
-			['Single Chest fill'] = function(t)
-				ItemInfo.Instance = ItemInfo()
-				ItemInfo.Instance:setStackSize('item', 64)
-
-				local chest = Chest(t.testName, Position(5, 5, 5, Position.SOUTH), false)
-
-				chest:push('item', chest:size() * 64)
-
-				t.assertEqual(chest:getItemAt(chest:size()), 'item')
-				t.assertEqual(chest:getItemCount(chest:size()), 64)
-				t.assertEqual(chest:getItemSpace(chest:size()), 0)
-				t.assertEqual(chest:getTotalSpaceFor('item'), 0)
-				t.assertEqual(chest:canPush('item', 1), false)
-				t.assertEqual(chest:push('item', 1), false)
-				t.assertEqual(chest:isEmpty(), false)
-
-				--TODO: move to a different test
-				t.assertEqual(chest:has('item'), true)
-
-				chest:remove()
+				local chest2 = Chest(p, true)
+				t.assertEqual(chest2:inventory():size(), 54)
 			end
-		},
-		['Chest Item Placement'] = function(t)
-			ItemInfo.Instance = ItemInfo()
-			ItemInfo.Instance:setStackSize('bucket', 16)
-			ItemInfo.Instance:setStackSize('cobblestone', 64)
-			ItemInfo.Instance:setStackSize('shovel', 1)
-
-			local chest = Chest(t.testName, Position(5, 5, 5, Position.SOUTH), false)
-			chest:clear()
-
-			chest:push('bucket', 33)
-			chest:push('cobblestone', 65)
-			chest:push('shovel', 4)
-
-			--Should result in chest as follows
-
-			-- bucket x 16
-			-- bucket x 16
-			-- bucket x 1
-			-- cobblestone x 64
-			-- cobblestone x 1
-			-- shovel x 1
-			-- shovel x 1
-			-- shovel x 1
-			-- shovel x 1
-
-			t.assertEqual(chest:getItemAt(1), 'bucket')
-			t.assertEqual(chest:getItemAt(2), 'bucket')
-			t.assertEqual(chest:getItemAt(3), 'bucket')
-			t.assertEqual(chest:getItemAt(4), 'cobblestone')
-			t.assertEqual(chest:getItemAt(5), 'cobblestone')
-			t.assertEqual(chest:getItemAt(6), 'shovel')
-			t.assertEqual(chest:getItemAt(7), 'shovel')
-			t.assertEqual(chest:getItemAt(8), 'shovel')
-			t.assertEqual(chest:getItemAt(9), 'shovel')
-			t.assertEqual(chest:getItemAt(10), nil)
-
-			--assert(chest:pop(3) == 'bucket', 'item not found in chest')
-			--assert(chest:pop(5) == 'cobblestone', 'item not found in chest')
-			--assert(chest:pop(9) == 'shovel', 'item not found in chest')
-			t.assertEqual(chest:getTotalSpaceFor('shovel'), (27 - 9) * ItemInfo.Instance:getStackSize('shovel'))
-			t.assertEqual(chest:getTotalSpaceFor('bucket'), 15 + (27 - 9) * ItemInfo.Instance:getStackSize('bucket'))
-			t.assertEqual(chest:getTotalSpaceFor('cobblestone'), 63 + (27 - 9) * ItemInfo.Instance:getStackSize('cobblestone'))
-
-			chest:remove()
-		end,
-		['Chest Pop'] = function(t)
-			ItemInfo.Instance = ItemInfo()
-			ItemInfo.Instance:setStackSize('bucket', 16)
-			ItemInfo.Instance:setStackSize('cobblestone', 64)
-			ItemInfo.Instance:setStackSize('shovel', 1)
-
-			local chest = Chest(t.testName, Position(5, 5, 5, Position.SOUTH), false)
-			chest:clear()
-
-			chest:push('bucket', 3)
-			chest:push('cobblestone', 2)
-			chest:push('shovel', 1)
-
-			--Should result in chest as follows
-
-			-- bucket x 1
-			-- cobblestone x 1
-			-- shovel x 1
-
-			t.assertEqual(chest:getItemCount(1), 3)
-			t.assertEqual(chest:getItemSpace(1), 13)
-			t.assertEqual(chest:peek(), 'bucket')
-			local b, bc = chest:pop()
-			t.assertEqual(b, 'bucket')
-			t.assertEqual(bc, 3)
-
-			t.assertEqual(chest:getItemCount(2), 2)
-			t.assertEqual(chest:getItemSpace(2), 62)
-			t.assertEqual(chest:peek(), 'cobblestone')
-			local c, cc = chest:pop()
-			t.assertEqual(c, 'cobblestone')
-			t.assertEqual(cc, 2)
-
-			t.assertEqual(chest:getItemCount(3), 1)
-			t.assertEqual(chest:getItemSpace(3), 0)
-			t.assertEqual(chest:peek(), 'shovel')
-			local s, sc = chest:pop()
-			t.assertEqual(s, 'shovel')
-			t.assertEqual(sc, 1)
-
-			chest:remove()
-		end
+		}
 	}
 )
