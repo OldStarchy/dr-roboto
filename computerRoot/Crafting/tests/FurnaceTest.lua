4040c2b03ce61b3acdde3c73141f5f1a6d54020d
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2019-03-07 00:51:48 +1030

    Add furnce test stub

--- computerRoot/Crafting/tests/FurnaceTest.lua
+++ computerRoot/Crafting/tests/FurnaceTest.lua
@@ -1,19 +1,2 @@
-test(
-	'Furnace',
-	{
-		Constructor = {
-			['Goto Top'] = function(t)
-				local location = Position(5, 5, 5, Position.SOUTH)
-				local obj = Furnace('furnace', location)
-
-				print(obj:toString())
-				obj:navigateTo()
-				t.assertEqual(nav:getX(), 5)
-				t.assertEqual(nav:getY(), 5)
-				t.assertEqual(nav:getZ(), 4)
-
-				obj:gotoTop()
-			end
-		}
-	}
-)
+test('Furnace', {})
+--TODO
