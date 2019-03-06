cfdb47565b4a10293740ecd39bb372caabcb51ca
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2019-03-06 23:09:41 +1030

    Add burn times to iteminfo

--- computerRoot/Inventory/ItemInfo.lua
+++ computerRoot/Inventory/ItemInfo.lua
@@ -2,7 +2,10 @@ ItemInfo = Class()
 ItemInfo.ClassName = 'ItemInfo'
 ItemInfo.Instance = nil
 
-ItemInfo._default = 64
+ItemInfo._default = {
+	stackSize = 64
+	--no burn time
+}
 
 function ItemInfo:constructor()
 	self._data = {}
@@ -18,41 +21,73 @@ function ItemInfo.Deserialize(obj)
 	assertType(obj, 'table')
 
 	local ii = ItemInfo()
+	ii.ev:suppress(true)
 
 	for i, v in pairs(obj) do
-		ii:setStackSize(i, v)
+		assertType(v, 'table')
+
+		if (v.stackSize ~= nil) then
+			ii:setStackSize(i, v.stackSize)
+		end
+
+		if (v.burnTime ~= nil) then
+			ii:setBurnTime(i, v.burnTime)
+		end
 	end
 
+	ii.ev:suppress(false)
 	return ii
 end
 
-function ItemInfo:getStackSize(item)
+function ItemInfo:_get(item, key)
 	if (type(item) == 'string') then
 		item = ItemDetail.FromId(item)
 	end
 
 	if (item == nil) then
-		return self._default
+		return self._default[key]
 	end
 
 	for name, size in pairs(self._data) do
 		if (item:matches(name)) then
-			return self._data[name]
+			return self._data[name][key]
 		end
 	end
 
-	return self._default
+	return self._default[key]
 end
 
-function ItemInfo:setStackSize(itemSelector, size)
+function ItemInfo:_set(itemSelector, key, value)
 	assertType(itemSelector, 'string')
-	assertType(size, 'int')
+	assertType(value, 'int')
 
-	if (size <= 0) then
-		error('Invalid stack size "' .. tostring(size) .. '"', 2)
+	if (value <= 0) then
+		error('Invalid "' .. key .. '" "' .. tostring(value) .. '"', 2)
 	end
 
 	itemSelector = ItemDetail.NormalizeId(itemSelector)
 
-	self._data[itemSelector] = size
+	if (self._data[itemSelector] == nil) then
+		self._data[itemSelector] = {}
+	end
+
+	self._data[itemSelector][key] = value
+
+	self.ev:trigger('state_changed')
+end
+
+function ItemInfo:getStackSize(item)
+	return self:_get(item, 'stackSize')
+end
+
+function ItemInfo:getBurnTime(item)
+	return self:_get(item, 'burnTime')
+end
+
+function ItemInfo:setStackSize(itemSelector, size)
+	self:_set(itemSelector, 'stackSize', size)
+end
+
+function ItemInfo:setBurnTime(itemSelector, time)
+	self:_set(itemSelector, 'burnTime', time)
 end
--- computerRoot/Inventory/tests/ItemInfoTest.lua
+++ computerRoot/Inventory/tests/ItemInfoTest.lua
@@ -4,13 +4,13 @@ test(
 		['Get / Set'] = function(t)
 			local ii = ItemInfo()
 
+			ii:setStackSize('lava_bucket:*', 1)
+			ii:setBurnTime('lava_bucket:*', 1000)
+
 			t.assertEqual(ii:getStackSize('missing'), 64)
-
-			ii:setStackSize('plank:*', 16)
-
-			t.assertEqual(ii:getStackSize('plank:4'), 16)
-
-			t.assertEqual(ii:getStackSize('plank'), 16)
+			t.assertEqual(ii:getBurnTime('missing'), nil)
+			t.assertEqual(ii:getStackSize('lava_bucket:0'), 1)
+			t.assertEqual(ii:getBurnTime('lava_bucket:0'), 1000)
 		end
 	}
 )
