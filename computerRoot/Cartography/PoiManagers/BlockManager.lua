3a57d8c406570db86821c75ee3fc51e05c164232
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2019-03-09 02:06:54 +1030

    Remove old block type differenciation
    Blocks were separated by type because there didn't used to be a way to
    serialize them and record types.

--- computerRoot/Cartography/PoiManagers/BlockManager.lua
+++ computerRoot/Cartography/PoiManagers/BlockManager.lua
@@ -17,26 +17,15 @@ function BlockManager:constructor(map)
 	self.ev = EventManager()
 end
 
-function BlockManager.Deserialize(data)
-	local map = BlockManager()
-	for blockType, _ in ipairs(data) do
-		for _, blockData in ipairs(data[key]) do
-			block = _G[blockType].ConvertToInstance(blockData)
-			block.location = Position.ConvertToInstance(block.location)
-			block.interfaceLocation = Position.ConvertToInstance(block.interfaceLocation)
-			-- print(block.location:toString())
+function BlockManager.Deserialize(tbl)
+	local obj = BlockManager(Map.Instance)
 
-			local blockList = map._blocks[blockType]
-			if (blockList == nil) then
-				map._blocks[blockType] = {block}
-			else
-				table.insert(blockList, block)
-			end
-		end
+	for _, block in ipairs(tbl) do
+		obj:add(block)
 	end
 
-	-- print(tableToString(data))
-	return map
+	-- print(tableToString(tbl))
+	return obj
 end
 
 function BlockManager:serialize()
@@ -46,18 +35,13 @@ end
 function BlockManager:add(block)
 	assertType(block, Block, 'Attempt to add a non block to a block map', 2)
 
-	local blockType = block.ClassName
-
-	if (self:findBlockByLocation(blockType, block.location) ~= nil) then
+	if (self:findBlockByLocation(block.location) ~= nil) then
 		return false
 	end
 
-	local blockList = self._blocks[blockType]
-	if (blockList == nil) then
-		self._blocks[blockType] = {block}
-	else
-		table.insert(blockList, block)
-	end
+	table.insert(self._blocks, block)
+
+	self._map:setProtected(block.location, true)
 
 	self.ev:trigger('state_changed')
 
@@ -67,31 +51,24 @@ end
 function BlockManager:remove(block)
 	assertType(block, Block, 'Attempt to remove a non block to a block map', 2)
 
-	local blockType = block.ClassName
-
-	local blockInMap, key = self:findBlockByLocation(blockType, block.location)
+	local blockInMap, key = self:findBlockByLocation(block.location)
 	if (blockInMap == nil) then
 		return false
 	end
 
-	table.remove(self._blocks[blockType], key)
+	table.remove(self._blocks, key)
 
 	self.ev:trigger('state_changed')
 
 	return true
 end
 
-function BlockManager:findBlockByLocation(blockType, atPosition)
+function BlockManager:findBlockByLocation(atPosition)
 	assertType(atPosition, Position, 'Attempted to find a block by a location that wasnt a position object', 2)
-	assertType(blockType, 'string', 'Attempted to find a block by a block type that wasnt a string object', 2)
 
 	local position = atPosition
-	local blocksOfType = self._blocks[blockType]
-	if (blocksOfType == nil) then
-		return nil
-	end
 
-	for k, block in ipairs(blocksOfType) do
+	for k, block in ipairs(self._blocks) do
 		if (block.location:posEquals(position)) then
 			return block, k
 		end
@@ -102,20 +79,15 @@ end
 
 -- returns the closest block of that type to the provided location
 -- block type should be the class name of the block
-function BlockManager:findNearest(blockType, toLocation)
+function BlockManager:findNearest(toLocation)
 	assertType(toLocation, Position, 'Attempted to find a block by a location that wasnt a position object', 2)
-	assertType(blockType, 'string', 'Attempted to find a block by a block type that wasnt a string object', 2)
 
 	local other = toLocation
 
 	local currentSmallest = nil
 	local smallestDistance = 99999999
 
-	if (self._blocks[blockType] == nil) then
-		return nil
-	end
-
-	for _, block in ipairs(self._blocks[blockType]) do
+	for _, block in ipairs(self._blocks) do
 		local distance = block.location:distanceTo(other)
 		if distance < smallestDistance then
 			smallestDistance = distance
--- computerRoot/Cartography/PoiManagers/tests/BlockManagerTest.lua
+++ computerRoot/Cartography/PoiManagers/tests/BlockManagerTest.lua
@@ -2,82 +2,87 @@ test(
 	'BlockManager',
 	{
 		['Adding items'] = function(t)
-			local BlockManager = BlockManager()
+			local BlockManager = BlockManager(Map())
 
-			local furnace = Furnace('furnace', Position(5, 5, 5, Position.SOUTH))
+			local furnace = Furnace(Position(5, 5, 5, Position.SOUTH))
 			t.assertEqual(BlockManager:add(furnace), true)
 
-			local furnace2 = Furnace('furnace2', Position(5, 5, 4, Position.SOUTH))
+			local furnace2 = Furnace(Position(5, 5, 4, Position.SOUTH))
 			t.assertEqual(BlockManager:add(furnace2), true)
 
-			local chest = Chest('chest', Position(5, 5, 6, Position.SOUTH))
+			local chest = Chest(Position(5, 5, 6, Position.SOUTH), true)
 			t.assertEqual(BlockManager:add(chest), true)
 
-			t.assertTableEqual(BlockManager._blocks, {['Furnace'] = {furnace, furnace2}, ['Chest'] = {chest}})
+			t.assertTableEqual(BlockManager._blocks, {furnace, furnace2, chest})
 		end,
 		['Removing items'] = function(t)
-			local BlockManager = BlockManager()
+			local BlockManager = BlockManager(Map())
 
-			local furnace = Furnace('furnace', Position(5, 5, 5, Position.SOUTH))
+			local furnace = Furnace(Position(5, 5, 5, Position.SOUTH))
 			t.assertEqual(BlockManager:add(furnace), true)
 			t.assertEqual(BlockManager:remove(furnace), true)
 
-			t.assertTableEqual(BlockManager._blocks, {['Furnace'] = {}})
+			t.assertTableEqual(BlockManager._blocks, {})
 		end,
 		['Item Duplication'] = function(t)
-			local BlockManager = BlockManager()
+			local BlockManager = BlockManager(Map())
 
-			local furnace = Furnace('furnace', Position(5, 5, 5, Position.SOUTH))
+			local furnace = Furnace(Position(5, 5, 5, Position.SOUTH))
 			t.assertEqual(BlockManager:add(furnace), true)
 			t.assertEqual(BlockManager:add(furnace), false)
 
-			local chest = Chest('chest', Position(5, 5, 6, Position.SOUTH))
+			local chest = Chest(Position(5, 5, 6, Position.SOUTH), true)
 			t.assertEqual(BlockManager:add(chest), true)
 			t.assertEqual(BlockManager:add(chest), false)
 
-			t.assertTableEqual(BlockManager._blocks, {['Furnace'] = {furnace}, ['Chest'] = {chest}})
+			t.assertTableEqual(BlockManager._blocks, {furnace, chest})
 		end,
 		['Finding items'] = function(t)
-			local BlockManager = BlockManager()
+			local BlockManager = BlockManager(Map())
 
-			local furnace = Furnace('furnace', Position(5, 5, 5, Position.SOUTH))
+			local furnace = Furnace(Position(5, 5, 5, Position.SOUTH))
 			BlockManager:add(furnace)
 
-			local furnace2 = Furnace('furnace2', Position(5, 5, 4, Position.SOUTH))
+			local furnace2 = Furnace(Position(5, 5, 4, Position.SOUTH))
 			BlockManager:add(furnace2)
 
-			t.assertEqual(BlockManager:findBlockByLocation(furnace.ClassName, furnace.location), furnace)
-			t.assertEqual(BlockManager:findBlockByLocation(furnace2.ClassName, furnace2.location), furnace2)
+			t.assertEqual(BlockManager:findBlockByLocation(furnace.location), furnace)
+			t.assertEqual(BlockManager:findBlockByLocation(furnace2.location), furnace2)
 
-			t.assertEqual(BlockManager:findNearest(Furnace.ClassName, Position()), furnace2)
+			t.assertEqual(BlockManager:findNearest(Position()), furnace2)
 		end,
 		['Saving and loading items'] = function(t)
-			local BlockManager = BlockManager()
+			Map.Instance = Map()
+			local BlockManager = BlockManager(Map.Instance)
 
-			local furnace = Furnace('furnace', Position(5, 5, 5, Position.SOUTH))
+			local furnace = Furnace(Position(5, 5, 5, Position.SOUTH))
 			BlockManager:add(furnace)
 
-			local chest = Chest('chest', Position(5, 5, 6, Position.SOUTH))
+			local chest = Chest(Position(5, 5, 6, Position.SOUTH), true)
 			BlockManager:add(chest)
 
-			BlockManager:saveToFile('test.BlockManager')
+			local tbl = BlockManager:serialize()
 
-			local BlockManager2 = BlockManager.LoadFromFile('test.BlockManager', true)
+			local BlockManager2 = BlockManager.Deserialize(tbl)
 
-			t.assertTableEqual(BlockManager2._blocks, {['Furnace'] = {furnace}, ['Chest'] = {chest}})
+			t.assertTableEqual(BlockManager2._blocks, {furnace, chest})
 
 			BlockManager2:remove(chest)
 			BlockManager2:remove(furnace)
 
-			local BlockManager3 = BlockManager.LoadFromFile('test.BlockManager', true)
+			local tbl2 = BlockManager2:serialize()
 
-			t.assertTableEqual(BlockManager3._blocks, {['Furnace'] = {}, ['Chest'] = {}})
+			local BlockManager3 = BlockManager.Deserialize(tbl2)
+
+			t.assertTableEqual(BlockManager3._blocks, {})
 
 			BlockManager3:add(chest)
 			BlockManager3:add(furnace)
 
-			local BlockManager4 = BlockManager.LoadFromFile('test.BlockManager', true)
-			t.assertTableEqual(BlockManager4._blocks, {['Furnace'] = {furnace}, ['Chest'] = {chest}})
+			local tbl3 = BlockManager3:serialize()
+
+			local BlockManager4 = BlockManager.Deserialize(tbl3)
+			t.assertTableEqual(BlockManager4._blocks, {chest, furnace})
 		end
 	}
 )
