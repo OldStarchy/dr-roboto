1695a5ba519f141004c033b23f4d51e5509fa05f
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2019-03-08 22:52:03 +1030

    Add assertParameter helper function

--- computerRoot/roboto/component/types.lua
+++ computerRoot/roboto/component/types.lua
@@ -80,3 +80,30 @@ function assertType(obj, typ, err, frame)
 
 	return obj
 end
+
+function assertParameter(obj, name, ...)
+	local typs = {...}
+
+	for _, typ in ipairs(typs) do
+		if (isType(obj, typ)) then
+			return obj
+		end
+	end
+
+	local typsMsg = ''
+	for i, v in ipairs(typs) do
+		if (typsMsg == '') then
+			typsMsg = '"' .. tostring(v) .. '"'
+		elseif (#typs < 3) then
+			typsMsg = typsMsg .. ' or ' .. '"' .. tostring(v) .. '"'
+		elseif (i == #typs) then
+			typsMsg = typsMsg .. ', or ' .. '"' .. tostring(v) .. '"'
+		else
+			typsMsg = typsMsg .. ', ' .. '"' .. tostring(v) .. '"'
+		end
+	end
+
+	local errmsg = 'Parameter "' .. name .. '" must be ' .. typsMsg .. ', was "' .. type(obj) .. '"'
+
+	error(errmsg, 3)
+end
