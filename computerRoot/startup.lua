5f48c23874156bc62893a8e0dc3fabdac6baa2c7
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2022-08-14 23:08:19 +0930

    feat: add autorun directory

--- computerRoot/startup.lua
+++ computerRoot/startup.lua
@@ -1,3 +1,13 @@
 if (not loadfile('roboto.lua', _ENV)()) then
 	return
 end
+
+if (fs.exists('autorun')) then
+	local autorunners = fs.list('autorun')
+	for _, autorunner in ipairs(autorunners) do
+		local autorunnerPath = fs.combine('autorun', autorunner)
+		if (not fs.isDir(autorunnerPath)) then
+			shell.run(autorunnerPath)
+		end
+	end
+end
