b7d60bbacb30b900b954af22afa25d9c6c8accfd
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2022-08-14 23:40:01 +0930

    fix: less autocomplete using old filename

--- computerRoot/autorun/less.lua
+++ computerRoot/autorun/less.lua
@@ -1,5 +1,5 @@
 shell.setCompletionFunction(
-	'less',
+	'less.lua',
 	function(shell, index, text, previousText)
 		local commandWithoutLess = table.concat({select(2, unpack(previousText))}, ' ') .. ' ' .. text
 		return shell.complete(commandWithoutLess)
